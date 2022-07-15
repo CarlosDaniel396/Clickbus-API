135f735366a69559d7b5bff2585095057e701c69
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-07-15 09:55:11 -0300

    test: find-all-places-paged

--- backend/src/main/java/com/carlos/bus/services/PlaceService.java
+++ backend/src/main/java/com/carlos/bus/services/PlaceService.java
@@ -1,5 +1,25 @@
 package com.carlos.bus.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.carlos.bus.dtos.PlaceDTO;
+import com.carlos.bus.entities.Place;
+import com.carlos.bus.repositories.PlaceRepository;
+
+@Service
 public class PlaceService {
+
+	@Autowired
+	private PlaceRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<PlaceDTO> findAllPaged(String name, Pageable pageable) {
+		Page<Place> list = repository.find(name, pageable);
+		return list.map(x -> new PlaceDTO(x));
+	}
 
 }
---
+++ backend/src/test/java/com/carlos/bus/services/PlaceServiceTests.java
@@ -0,0 +1,64 @@
+package com.carlos.bus.services;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.carlos.bus.dtos.PlaceDTO;
+import com.carlos.bus.entities.Place;
+import com.carlos.bus.repositories.PlaceRepository;
+import com.carlos.bus.tests.Factory;
+
+
+@ExtendWith(SpringExtension.class)
+public class PlaceServiceTests {
+
+	@InjectMocks
+	private PlaceService service;
+
+	@Mock
+	private PlaceRepository repository;
+
+	private long existingId;
+	private long nonExistingId;
+	private Place place;
+	private PlaceDTO placeDTO;
+	private PageImpl<Place> page;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 2L;
+		place = Factory.createPlace();
+		placeDTO = Factory.createPlaceDTO();
+		page = new PageImpl<>(List.of(place));
+
+		Mockito.when(repository.findAll((Pageable) any())).thenReturn(page);
+
+		Mockito.when(repository.find(any(), any())).thenReturn(page);
+	}
+
+	@Test
+	public void findAllPagedShouldReturnPage() {
+
+		Pageable pageable = PageRequest.of(0, 10);
+
+		Page<PlaceDTO> result = service.findAllPaged("", pageable);
+
+		Assertions.assertNotNull(result);
+	}
+
+}
