2b8663b0fe28fbacd0bf5ed5a9b2fb7216c0d312
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-07-18 07:58:17 -0300

    feat: find-place-by-id

--- backend/src/main/java/com/carlos/bus/controllers/PlaceController.java
+++ backend/src/main/java/com/carlos/bus/controllers/PlaceController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,11 @@ public class PlaceController {
 		
 		Page<PlaceDTO> list = service.findAllPaged(name.trim(), pageable);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PlaceDTO> findById(@PathVariable Long id) {
+		PlaceDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
