2283ff925d9883dcf32fe890341e71e53b44e244
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-07-18 08:28:41 -0300

    test: delete-place

--- backend/src/test/java/com/carlos/bus/services/PlaceServiceTests.java
+++ backend/src/test/java/com/carlos/bus/services/PlaceServiceTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.carlos.bus.dtos.PlaceDTO;
 import com.carlos.bus.entities.Place;
 import com.carlos.bus.repositories.PlaceRepository;
+import com.carlos.bus.services.exceptions.DatabaseException;
 import com.carlos.bus.services.exceptions.ResourceNotFoundException;
 import com.carlos.bus.tests.Factory;
 
@@ -50,16 +52,19 @@ public class PlaceServiceTests {
 		page = new PageImpl<>(List.of(place));
 
 		Mockito.when(repository.findAll((Pageable) any())).thenReturn(page);
-		
+
 		Mockito.when(repository.find(any(), any())).thenReturn(page);
-	
+
 		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(place));
 		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
-		
+
 		Mockito.when(repository.getReferenceById(existingId)).thenReturn(place);
 		Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
 
 		Mockito.when(repository.save(any())).thenReturn(place);
+
+		Mockito.doNothing().when(repository).deleteById(existingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 	}
 
 	@Test
@@ -87,7 +92,7 @@ public class PlaceServiceTests {
 			service.findById(nonExistingId);
 		});
 	}
-	
+
 	@Test
 	public void updateShouldReturnPlaceDTOWhenIdExists() {
 
@@ -95,12 +100,28 @@ public class PlaceServiceTests {
 
 		Assertions.assertNotNull(result);
 	}
-	
+
 	@Test
 	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.update(nonExistingId, placeDTO);
+		});
+	}
+
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+	}
+
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
 		});
 	}
 }
